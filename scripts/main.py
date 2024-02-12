d3f8d34aa4571c3beb2ff46d6c760a905bf9f2d3
masarwy <mohammad.masarwy@gmail.com>
2024-02-12 11:30:23 +0200

    First infrastructure.

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 from object import Object
 from render import Render
@@ -8,7 +9,7 @@ import trimesh
 
 if __name__ == '__main__':
     obj_file = 'object.obj'
-    render_scene = True
+    render_scene = False
 
     obj = Object(obj_file)
     camera = Camera(radius=0.3)
@@ -29,5 +30,17 @@ if __name__ == '__main__':
 
     pose_generator = camera.generate_camera_poses(n_samples_theta=4, n_samples_phi=3)
 
+    counter = 0
     for pose in pose_generator:
-        _, _, mask = render.render_scene(mesh=obj.get_mesh(), camera_pose=pose.get_transformation_matrix())
+        _, depth, mask = render.render_scene(mesh=obj.get_mesh(), camera_pose=pose.get_transformation_matrix())
+        image_min = depth.min()
+        image_max = depth.max()
+
+        # Normalize to 0-1 range
+        normalized_image = (depth - image_min) / (image_max - image_min)
+
+        # Scale to 0-255 and convert to uint8
+        scaled_image = (normalized_image * 255).astype(np.uint8)
+        image = Image.fromarray(scaled_image)
+        image.save(f'../data/img/di_{counter}.png')
+        counter += 1
---
+++ scripts/sim_matrix.py
@@ -0,0 +1,29 @@
+import glob
+from skimage.metrics import structural_similarity as ssim
+from skimage.io import imread
+import numpy as np
+
+def load_and_preprocess_image(file_path):
+    image = imread(file_path)
+    return image
+
+if __name__ == '__main__':
+    file_pattern = '../data/img/di_*.png'
+    image_files = sorted(glob.glob(file_pattern))
+
+    if not image_files:
+        raise ValueError("No image files found. Check the file pattern or directory.")
+
+    images = [load_and_preprocess_image(file) for file in image_files]
+
+    n_images = len(images)
+    similarity_matrix = np.zeros((n_images, n_images))
+
+    for i in range(n_images):
+        for j in range(i, n_images):
+            if i == j:
+                similarity_matrix[i, j] = 1
+            else:
+                score, _ = ssim(images[i], images[j], full=True)
+                similarity_matrix[i, j] = similarity_matrix[j, i] = score
+    print(similarity_matrix)
